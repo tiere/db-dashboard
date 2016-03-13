b76f89c51d71ce8a63f7f7bdf594b716a9d7fc92
Tommi Järvinen <tommi.tiere@gmail.com>
2016-03-13 17:35:30 +0200

    Style and script compilation
    - Compile, concat and minify styles
    - Concat and minify scripts
    - Add watch tasks for styles and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
-var gulp = require('gulp');
+(function () {
+  'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+  var cleanCSS = require('gulp-clean-css');
+  var concat   = require('gulp-concat');
+  var gulp     = require('gulp');
+  var sass     = require('gulp-sass');
+  var uglify   = require('gulp-uglify');
+
+  gulp.task('default', function () {
+    gulp.start('styles', 'scripts');
+  });
+
+  gulp.task('watch', ['styles', 'scripts'], function () {
+    gulp.watch('./assets/stylesheets/**/*.scss', ['styles']);
+    gulp.watch('./assets/javascripts/**/*.js', ['scripts']);
+  });
+
+  gulp.task('styles', function () {
+    return gulp.src('./assets/stylesheets/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(concat('all.css'))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('./public/stylesheets'));
+  });
+
+  gulp.task('scripts', function () {
+    return gulp.src('./assets/javascripts/**/*.js')
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+      .pipe(gulp.dest('./public/javascripts'));
+  });
+}());
