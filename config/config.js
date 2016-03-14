3103c2d0ae6d5e7892bc4e124c7d4442d65907d1
Tommi Järvinen <tommi.tiere@gmail.com>
2016-03-14 22:10:29 +0200

    Add file for configuration options

---
+++ config/config.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var config = {
+  db: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
+};
+
+module.exports = config;
