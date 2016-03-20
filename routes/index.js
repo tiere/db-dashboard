88b59916a4cf0b9442f13972865df02f8252af19
Tommi Järvinen <tommi.tiere@gmail.com>
2016-03-20 22:06:56 +0200

    Add route for table rows

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/tables/:table/rows', function (req, res, next) {
+  var table = res.locals.tables.find(x => x.name === req.params.table);
+
+  table.rows.then((result) => {
+    res.json(result);
+  });
 });
 
 module.exports = router;
